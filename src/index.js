87eb89fcdcc7b16299ef5385195ebde72ff6f035
dbyers24 <dbyers84@gmail.com>
2017-09-29 22:10:16 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_Key = 'AIzaSyCPm8DJKOnTgxVhbEGf7EHfpf6rvPTUjgc';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div >
+    <
+    SearchBar / >
+    <
+    /div>
+}
+
+ReactDOM.render( < App / > , document.querySelector( '.container' ) );
