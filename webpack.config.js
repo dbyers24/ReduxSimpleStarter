4892fac81309aced52886a1fb55ee0a2abddb2f6
Dustin Byers <dbyers84@gmail.com>
2017-10-07 16:43:45 -0700

    cheetos

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const production = process.env.NODE_ENV === 'production';
+
+let plugins = [
+	new EnvironmentPlugin(['NODE_ENV']),
+	new ExtractPlugin('bundle.[hash].css'),
+	new HTMLPlugin({ template: `${__dirname}/src/index.html` }),
+	new DefinePlugin({
+		__DEBUG__: JSON.stringify(!production),
+		__API__URL: JSON.stringify(process.env.API_URL),
+	}),
+];
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -7,6 +19,7 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  devtool: production ? undefined : `cheap-module-eval-source-map`,
   module: {
     loaders: [{
       exclude: /node_modules/,
